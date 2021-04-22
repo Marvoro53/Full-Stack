f164a2bd91569068aea8669d72587c8558183164
Marvoro53 <marvinlara321@gmail.com>
2021-04-22 17:05:56 -0600

    added data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Server Setup
 const express = require("express");
 const app = express();
+//body parser
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+//
+//Port
 const PORT = 3000;
+//
+
 //Start Server 
-app.listen(PORT, () => console.log(`notes-app listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Book app listening on port ${PORT}`));
 //
