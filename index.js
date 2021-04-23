91e0904227cf3baee8bebd03b1d53d5fa10287c2
Marvoro53 <marvinlara321@gmail.com>
2021-04-22 18:52:46 -0600

    routes

--- index.js
+++ index.js
@@ -5,10 +5,39 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 //
+var path= require('path');
+//pug set up
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+//
 //Port
 const PORT = 3000;
 //
+//routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.get("/", function(req, res){
+    res.render('home');
+});
+
+app.get("/action", function(req, res){
+    res.render('action')
+});
+
+app.get("/fantasy", function(req, res){
+    res.render('fantasy')
+});
+
+app.get("/mistery", function(req,res){
+    res.render("mistery")
+});
+
+app.get("/romance", function(req,res){
+    res.render("romance")
+});
+
+//
 //Start Server 
 app.listen(PORT, () => console.log(`Book app listening on port ${PORT}`));
 //
